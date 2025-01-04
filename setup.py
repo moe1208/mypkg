0ccc072186d5fda50e0b51ece556ce828d1911b5
moe1208 <w26qp0da7pq2tis6s3kb@docomo.ne.jp>
2025-01-05 02:36:44 +0900

    write

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'talker = mypkg.talker:main',
+            'age = mypkg.age:main',
             'listener = mypkg.listener:main',
         ],
     },
